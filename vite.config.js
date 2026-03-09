35f3fa4de03ba63c9c671d71d4c65229b8515412
TMH611205 <minhhoang11062005@gmail.com>
2026-03-09 10:38:42 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    root: 'src',
+    root: '.',
     plugins: [],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY || ''),
