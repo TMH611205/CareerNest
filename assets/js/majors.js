063ff6982a067db773281ec4af1441f760f7f36e
TMH611205 <minhhoang11062005@gmail.com>
2026-03-29 14:52:16 +0700

    fix ngành học

--- assets/js/majors.js
+++ assets/js/majors.js
@@ -1,7 +1,14 @@
+
+function shortText(text) {
+  if (!text) return '';
+  return text.length > 50 ? text.substring(0, 50) + '...' : text;
+}
+
+
 async function loadMajors() {
   const res = await fetch('http://localhost:9999/CareerNest/CareerNest_Backend/api/majors.php');
   const data = await res.json();
-
+  
   const tbody = document.querySelector('#table-majors tbody');
 
   tbody.innerHTML = data.map(m => {
@@ -9,21 +16,26 @@ async function loadMajors() {
     ? `${Number(m.SalaryMin).toLocaleString()} - ${Number(m.SalaryMax).toLocaleString()} VND`
     : "Chưa cập nhật";
 
+    const safe = encodeURIComponent(JSON.stringify(m));
+
   return  `
     <tr>
-      <td>${m.CareerCode}</td>
-      <td>${m.CareerName}</td>
-      <td>${m.Category || ''}</td>
-      <td>${salary}</td>
-      <td>0</td>
+        <td>${m.CareerCode}</td>
+        <td>${m.CareerName}</td>
+        <td>${m.Category || ''}</td>
+        <td>${salary}</td>
+    //    <td>${m.Description ? shortText(m.Description) : ''}</td>
+    //   <td>${m.Highlight || ''}</td>
+    //   <td>${renderDemand(m.DemandLevel)}</td>
+    //   <td>${m.Rating ?? 0} (${m.RatingCount ?? 0})</td>     
+      <td>${m.Views ?? 0}</td>
     <td>
-      <button class="action-btn" title="Xem" onclick='openModal("major", ${JSON.stringify(m)}, true)'><i data-lucide="eye" style="width:14px"></i></button>
-      <button class="action-btn" title="Sửa" onclick='openModal("major", ${JSON.stringify(m)})'><i data-lucide="edit-2" style="width:14px"></i></button>
+      <button class="action-btn" title="Xem" onclick="openModal('major', JSON.parse(decodeURIComponent('${safe}')), true)"><i data-lucide="eye" style="width:14px"></i></button>
+      <button class="action-btn" title="Sửa" onclick="openModal('major', JSON.parse(decodeURIComponent('${safe}')))"><i data-lucide="edit-2" style="width:14px"></i></button>
       <button class="action-btn" style="color: #ef4444;" title="Xóa" onclick="deleteMajor(${m.CareerID})"><i data-lucide="trash-2" style="width:14px"></i></button>
     </td>
   </tr>
 `}).join('');  
-
 
     lucide.createIcons();
 }
